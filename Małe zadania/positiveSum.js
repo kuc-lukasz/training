174e211af21d81a3de876d10896512bdd9e992d5
Luka Ku <lukaszkuc93@gmail.com>
2021-10-26 13:25:58 +0200

    writeOppositnumb

--- Małe zadania/positiveSum.js
+++ Małe zadania/positiveSum.js
@@ -1,7 +1,9 @@
 // Stwórz funkcję która obliczy sumę liczb dodatnich 
+// Najpierw filter powoduje że wybieramy tylko liczby dodatnie, następnie reduce sumuje nam liczby 
 
 const arrayNumb = [3,4,5,1,3,-2,5,-1,-2]
 
+// Pierwsza metoda
 // function positiveNumb(array) {
 //   const newArray = array.filter((e)=> {
 //       return e > 0     
@@ -11,6 +13,7 @@ const arrayNumb = [3,4,5,1,3,-2,5,-1,-2]
 // },0)
 // }
 
+//Druga metoda 
 function positiveNumb(array) {
  const newArray = array.filter((e) => e > 0).reduce((prv, curr) => {return prv + curr})
  return newArray
---
+++ Małe zadania/writeOppositNumb.js
@@ -0,0 +1,5 @@
+function oppositeNumb (numb) {
+    return -numb
+}
+
+console.log(oppositeNumb(-4))
