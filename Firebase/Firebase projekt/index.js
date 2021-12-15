89ae6a3b8b4e29b889728280eb9a784c973ad835
Luka Ku <lukaszkuc93@gmail.com>
2021-12-15 17:14:14 +0100

    add firestore collection to DOM

--- Firebase/Firebase projekt/index.js
+++ Firebase/Firebase projekt/index.js
@@ -1,3 +1,6 @@
+const cafeList = document.querySelector('#cafe-list')
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDmvM4DB62CW24nFhbuOfFQ-Tn2mW3h2mk",
     authDomain: "todo-firebase-82b06.firebaseapp.com",
@@ -9,5 +12,30 @@ const firebaseConfig = {
 
   firebase.initializeApp(firebaseConfig)
   
-  db = firebase.firestore()
+  // stworzenie bazy danych db=database
+   db = firebase.firestore()
+  db.settings({timestampsInSnapshots: true})
+ 
+//stworzenie elementu i renderowanie cafe
+function renderCafe(doc){
+let li = document.createElement('li')
+let name = document.createElement('span')
+let city = document.createElement('span')
 
+ li.setAttribute('data-id', doc.id)
+ name.innerText = doc.data().name
+ city.innerText = doc.data().city
+
+ li.appendChild(name)
+ li.appendChild(city)
+
+ cafeList.appendChild(li)
+
+
+}
+
+db.collection('Cafes').get().then((snapshot)  => {
+    snapshot.docs.forEach((doc) => {
+        renderCafe(doc)
+    })
+})
