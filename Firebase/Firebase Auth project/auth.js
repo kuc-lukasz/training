b353c35b9f8de9d5bbc13f853fc8a4a0620b4588
Luka Ku <lukaszkuc93@gmail.com>
2021-12-16 15:49:16 +0100

    add auth rules and hide content when user in log out

--- Firebase/Firebase Auth project/auth.js
+++ Firebase/Firebase Auth project/auth.js
@@ -1,51 +1,60 @@
-//sluchanie na status autoryzacji, tutaj bedziemy zawierac wszystkie reakcje zaleznie od statusu uzytkownika 
+//sluchanie na status autoryzacji, tutaj bedziemy zawierac wszystkie reakcje zaleznie od statusu uzytkownika
 
-auth.onAuthStateChanged(user => {
-    if(user){
-        window.alert('jestes zalogowany')
-    } else {
-        window.alert('jestes wylogowany')
-    }
-})
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // pobranie dokumentow z firestore
+    db.collection("guides")
+      .get()
+      .then((snapshot) => {
+        setUpGuides(snapshot.docs);
+      });
+  } else {
+    setUpGuides([]);
+  }
+});
 
 //Sign up
-const formSignUp = document.querySelector('#signup-form')
+const formSignUp = document.querySelector("#signup-form");
 
-formSignUp.addEventListener('submit', (e) => {
-    e.preventDefault()
+formSignUp.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    //pobranie danych z inputow
-    const email = document.querySelector('#signup-email').value;
-    const password = document.querySelector('#signup-password').value;
+  //pobranie danych z inputow
+  const email = document.querySelector("#signup-email").value;
+  const password = document.querySelector("#signup-password").value;
 
-    //zarejestrowanie danych w bazie firebase 
-    auth.createUserWithEmailAndPassword(email, password).then((userCredential) => {
+  //zarejestrowanie danych w bazie firebase
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const modal = document.querySelector("#modal-signup");
+      M.Modal.getInstance(modal).close();
+      formSignUp.reset();
+    });
+});
 
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-        formSignUp.reset()
-    })   
-})
+//Log-out
+const logOut = document.querySelector("#logout");
+logOut.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
 
-//Log-out 
-const logOut = document.querySelector('#logout')
-logOut.addEventListener('click', (e) => {
-e.preventDefault()
-    auth.signOut()
-})
+// log-In
+const logInForm = document.querySelector("#modal-login");
 
-// log-In 
-const logInForm = document.querySelector('#modal-login')
+logInForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //pobranie danych z inputow
+  const email = document.querySelector("#login-email").value;
+  const password = document.querySelector("#login-password").value;
 
-logInForm.addEventListener('submit', (e) => {
-e.preventDefault()
-//pobranie danych z inputow
-const email = document.querySelector('#login-email').value;
-const password = document.querySelector('#login-password').value;
-
-    auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        formSignUp.reset()
-    }).catch((error) => { });
-})
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const modal = document.querySelector("#modal-login");
+      M.Modal.getInstance(modal).close();
+      formSignUp.reset();
+    })
+    .catch((error) => {});
+});
--- Firebase/Firebase Auth project/index.js
+++ Firebase/Firebase Auth project/index.js
@@ -1,15 +1,37 @@
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
+const guideList = document.querySelector(".guides");
 
-    // kliknięcie we wszystkie elementy z klasą "modal" spowodują otworzenie konkretnego panelu. Konkretnemu modułowi dajemy id które wpisujemy następnie w w data-target w nawigacji. Dzięki temu klikając w nawigacji w dany element odrazu otworzy nam przypisany modul
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-  
-    //podobnie jak wyżej, kliknięcie w konkretny ul rozwinie nam li
-    var items = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(items);
-  
-  });
+//setup guides Najpierw jest obiekt -> tablica z
+const setUpGuides = (data) => {
+  let html = "";
+
+  if (data.length) {
+    data.forEach((doc) => {
+      const guide = doc.data();
+
+      const li = `
+  <li>
+          <div class="collapsible-header grey lighten-4">${guide.title}</div>
+          <div class="collapsible-body white">${guide.content}</div>
+  </li>
+  `;
+      html += li;
+    });
+    guideList.innerHTML = html;
+  } else {
+    guideList.innerHTML = `<h5 class="center-align">Login to view guides</h5>`
+  }
+};
+
+// setup materialize components
+document.addEventListener("DOMContentLoaded", function () {
+  // kliknięcie we wszystkie elementy z klasą "modal" spowodują otworzenie konkretnego panelu. Konkretnemu modułowi dajemy id które wpisujemy następnie w w data-target w nawigacji. Dzięki temu klikając w nawigacji w dany element odrazu otworzy nam przypisany modul
+  var modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
+
+  //podobnie jak wyżej, kliknięcie w konkretny ul rozwinie nam li
+  var items = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(items);
+});
 
 const firebaseConfig = {
   apiKey: "AIzaSyBjOk2kgwIUxKp-P6Cj13JsriKf9zceGQY",
@@ -17,13 +39,13 @@ const firebaseConfig = {
   projectId: "auth-project-648e0",
   storageBucket: "auth-project-648e0.appspot.com",
   messagingSenderId: "902673576335",
-  appId: "1:902673576335:web:3807e61ef7cdb1fbe15bfa", 
+  appId: "1:902673576335:web:3807e61ef7cdb1fbe15bfa",
 };
-// to inicjalizuje nasze połaczenie z bazą firebase 
+// to inicjalizuje nasze połaczenie z bazą firebase
 firebase.initializeApp(firebaseConfig);
 
-// inicjalizacja autoryzacji 
+// inicjalizacja autoryzacji
 const auth = firebase.auth();
 
-// inicjalizacja bazy danych 
+// inicjalizacja bazy danych
 db = firebase.firestore();
