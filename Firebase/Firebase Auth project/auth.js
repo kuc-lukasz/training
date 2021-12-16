b15393e6e6175fcf0e90e16891549b083733b533
Luka Ku <lukaszkuc93@gmail.com>
2021-12-16 12:10:11 +0100

    logout and login function

--- Firebase/Firebase Auth project/auth.js
+++ Firebase/Firebase Auth project/auth.js
@@ -10,13 +10,38 @@ formSignUp.addEventListener('submit', (e) => {
 
     //zarejestrowanie danych w bazie firebase 
     auth.createUserWithEmailAndPassword(email, password).then((userCredential) => {
-        console.log(userCredential)
 
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         formSignUp.reset()
+    })   
+})
 
-    })
-     
-     
+//Log-out 
+const logOut = document.querySelector('#logout')
+logOut.addEventListener('click', (e) => {
+e.preventDefault()
+    auth.signOut().then(() => {
+
+        window.alert('Zostałeś wylogowany');
+
+        }).catch((error) => {
+
+        console.error(error.message)
+        });
+    
+})
+
+// log-In
+
+const logIn = document.querySelector('#modal-login')
+logIn.addEventListener('click', (e) => {
+e.preventDefault()
+
+    auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+
+    var user = userCredential.user;
+    console.log(user);
+
+    }).catch((error) => { });
 })
