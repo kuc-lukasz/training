d2b5cc4d43a4f89d5bb2a2b044430f4b1c09d940
Luka Ku <lukaszkuc93@gmail.com>
2021-12-21 13:02:04 +0100

    use fetch and display data in dom

--- toDo app/index.js
+++ toDo app/index.js
@@ -1,18 +1,52 @@
 const task = document.querySelector('.task')
-const allTask = document.querySelectorAll('.task')
+const tasks = document.querySelector('.task')
 const addBtn = document.querySelector('.add-btn')
+const trash = document.querySelectorAll('#trash')
+
+
+function addTask () {
+    const newTask = prompt('Wpisz zadanie')
+    const newLi = document.createElement('li')
+    const inputCheck = document.createElement('input')
+    const label = document.createElement('label')
+    const trash = document.createElement('button')
+    
+    newLi.classList.add('task')
+   
+    inputCheck.type = 'checkbox'
+    inputCheck.id = "task-1"
+    label.setAttribute('for',"task-1" )
+    trash.setAttribute("id","trash")
+    trash.setAttribute("class","trash")
+    
+
+    label.innerText = newTask
+
+    newLi.appendChild(inputCheck)
+    newLi.appendChild(label)
+    newLi.appendChild(trash)
+    
+
+    allTask.appendChild(newLi)
+}
 
 addBtn.addEventListener('click', ()=>{
-    console.log('dziala')
+    addTask()
+
 })
 
-allTask.forEach((task)=>{
-    const trash = task.querySelector('.trash')
+function deleteTask(){
+    tasks.forEach((e)=>{
+        let trash = tasks['.trash']
 
-    function removeTasks (){
-        task.remove()
-    }
+        trash.addEventListener('click', ()=> {
+            console.log('dziala')
+        })
+    })
 
-    trash.addEventListener('click', removeTasks)
-})
+    
+}
+
+
+
 
