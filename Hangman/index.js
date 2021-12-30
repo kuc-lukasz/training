7a6e15931a6614fced9ea62062b6b76712fdfb71
Luka Ku <lukaszkuc93@gmail.com>
2021-12-30 15:34:07 +0100

    add functions which check all letters and display information and verify everything

--- Hangman/index.js
+++ Hangman/index.js
@@ -1,5 +1,50 @@
 let word = "html"
 
+let wordsUsed = []
+
+let guessedLetters = 0
+
+let lifes = 10
+
+
 function checkTheLetter (letter, word) {
     return word.includes(letter)
+}
+
+
+function howManyTimeOccurs (letter, word) {
+    return [...word].filter(singleword => singleword === letter).length
+}
+
+function writeLetter (letter) {
+
+    if(wordsUsed.includes(letter)) {
+        throw new Error('This wrote this later before!')
+
+    }
+
+    wordsUsed.push(letter)
+
+    console.log(wordsUsed)
+
+    let occurs = howManyTimeOccurs(letter, word)
+    
+    
+
+    if(checkTheLetter(letter, word)){
+        guessedLetters += occurs
+
+        console.log(guessedLetters)
+    } else {
+        lifes -= 1
+    }
+
+    if (guessedLetters === word.length){
+        window.alert('You WIN!')
+    }
+
+    if(lifes === 0){
+        window.alert('You Lose My Dear :(')
+    }
+
 }
