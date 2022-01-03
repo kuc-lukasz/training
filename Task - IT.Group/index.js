1363f37750f6a381877436c74cac6b48c79ecf84
Luka Ku <lukaszkuc93@gmail.com>
2022-01-03 15:09:06 +0100

    add new html and functions filter, sort and forEach

--- Task - IT.Group/index.js
+++ Task - IT.Group/index.js
@@ -1,30 +1,44 @@
-const listOfStudents = document.querySelector('#listOfNumbers')
-const button = document.querySelector('#button')
+const eventNumb = document.querySelector("#evenNumb");
+const oddNumb = document.querySelector("#oddNumb");
+const button = document.querySelector("#button");
 
-const theadTag = document.createElement('thead')
-  const trTag = document.createElement('tr')
-  const id = document.createElement('th')
+button.addEventListener("click", (e) => {
+  e.preventDefault();
 
-  button.addEventListener('click', (e) => {
-    e.preventDefault()
+  eventNumb.innerHTML = "";
+  oddNumb.innerHTML = "";
 
-    function getRandomInt(min, max) {
-
-    return Array.from({ length: 20 }, () => Math.floor(Math.random() * (max - min)) + min)
-
-      }
-
-    const randomInt = getRandomInt(1,101)
-    console.log(randomInt)
-
-    const odd = randomInt.filter((e) => e % 2 === 0)
-    const even = randomInt.filter((e) => e % 2 !== 0)
-
-   console.log(odd)
-   console.log(even)
- 
-
+  function getRandomInt(min, max) {
+    return Array.from(
+      { length: 20 },
+      () => Math.floor(Math.random() * (max - min)) + min
+    );
   }
-  
-  )
 
+  const randomInt = getRandomInt(1, 101);
+
+  const odd = () =>
+    randomInt
+      .filter((e) => e % 2 === 0)
+      .sort((a,b) => a > b? 1:-1)
+      .forEach((e) => {
+        let oddElement = document.createElement("td");
+
+        oddElement.innerText = e;
+
+        oddNumb.appendChild(oddElement);
+      });
+
+  const even = () =>
+    randomInt
+      .filter((e) => e % 2 !== 0)
+      .sort((a,b) => a > b? 1:-1)
+      .forEach((e) => {
+        let evenElement = document.createElement("td");
+        evenElement.innerText = e;
+        eventNumb.appendChild(evenElement);
+      });
+
+  even();
+  odd();
+});
