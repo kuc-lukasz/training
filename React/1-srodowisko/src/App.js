0ba3109d24321dcbdcd7ad9220110829bba941e8
Luka Ku <lukaszkuc93@gmail.com>
2022-01-10 12:51:30 +0100

    working with useState

--- React/1-srodowisko/src/App.js
+++ React/1-srodowisko/src/App.js
@@ -1,29 +1,70 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
-const name = "Łukaszzzz";
+const DEFAULT_COUNT = 0;
 
-const getAge = () => {
-  return 30;
+const NewComponent = () => {
+  const [count, setCount] = useState(DEFAULT_COUNT);
+
+  return (
+    <div>
+      <h1>Jestem nowym komponentem</h1>
+      <h5> {count} </h5>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Dodaj{" "}
+      </button>
+    </div>
+  );
 };
 
 function App() {
+  const [count, setCount] = useState(DEFAULT_COUNT);
+
+  const name = "Łukaszzzz";
+
+  const getAge = () => {
+    return 30;
+  };
+
+  //listener onClick
+  //handleClick
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        {" "}
-        {name}
-        <h2>Mam dokładnie: {getAge()} lat </h2>
-        <a
-          className="App-link"
-          href="https://github.com/kuc-lukasz?tab=repositories"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Link
-        </a>
-        <p>Witaj {name}</p>
-      </header>
+      {" "}
+      {name}
+      <h2>Mam dokładnie: {getAge()} lat </h2>
+      <a
+        className="App-link"
+        href="https://github.com/kuc-lukasz?tab=repositories"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github Link
+      </a>
+      <p>Witaj {name}</p>
+      <div>Obecna ilość kliknięć to: {count}</div>
+      <div>
+        <button onClick={handleSubtract}>Odejmij 1</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleAdd}>Dodaj 1</button>
+      </div>
+      <NewComponent />
     </div>
   );
 }
