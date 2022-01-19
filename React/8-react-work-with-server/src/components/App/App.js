da6e6221d0cac27fdde2446f2c24e6df265e8cc2
Luka Ku <lukaszkuc93@gmail.com>
2022-01-19 12:39:25 +0100

    add function stopInterval

--- React/8-react-work-with-server/src/components/App/App.js
+++ React/8-react-work-with-server/src/components/App/App.js
@@ -4,8 +4,9 @@ import "./App.css";
 function App() {
   // isloading sprawi że wyświetlni nam cos w trakcie ladowania listy. Implementujemy ją najpierw przed fetchem a potem na końcu już po odebraniu danych oraz już po returnie
   const [isLoading, setIsLoading] = useState(false);
-  const [workers, setWorkes] = useState([]);
+  const [workers, setWorkes] = useState(null);
   const [refresh, setRefresh] = useState(0);
+  const [syncEnabled, setSyncEnabled] = useState(true);
 
   //pierwszy parametr useEffect to funkcja, drugi to tablica zależności - sa jej trzy opcje
   // 1. może jej nie być, to sprawi że useEffect odpali sie za kazdym razem kiedy komponent sie renderuje
@@ -28,18 +29,33 @@ function App() {
   //możemy dodatkowo sprawić za pomocą setInterval aby odświeżało liste co jakiś konkretny czas, do tego musimy stworzyć nowego useState
 
   useEffect(() => {
-    setInterval(() => {
+    if (syncEnabled === false) {
+      return;
+    }
+
+    let intervalId = setInterval(() => {
       setRefresh((old) => old + 1);
-    }, 10000);
-  }, []);
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [syncEnabled]);
 
   return (
     <div className="App">
       <section>
         <h2>Employes</h2>
-        {isLoading && <p>Loading...</p>}
+        <button
+          onClick={() => {
+            setSyncEnabled(false);
+          }}
+        >
+          Stop refreshing
+        </button>
+        {workers === 0 && isLoading && <p>Loading...</p>}
         <ul>
-          {workers.map((worker) => {
+          {workers?.map((worker) => {
             return (
               <>
                 <li key={worker.id}>{worker.name}</li>
