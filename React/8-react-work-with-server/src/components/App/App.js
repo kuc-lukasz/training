ce042e9584703f69b0bd6504b72f10818f172b5b
Luka Ku <lukaszkuc93@gmail.com>
2022-01-19 12:05:55 +0100

    add automaticlly refreshing with data.json

--- React/8-react-work-with-server/src/components/App/App.js
+++ React/8-react-work-with-server/src/components/App/App.js
@@ -1,24 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const employes = [
-  {
-    id: 1,
-    name: "Jose",
-  },
-  {
-    id: 2,
-    name: "Steve",
-  },
-  {
-    id: 3,
-    name: "Henry",
-  },
-];
 function App() {
   // isloading sprawi że wyświetlni nam cos w trakcie ladowania listy. Implementujemy ją najpierw przed fetchem a potem na końcu już po odebraniu danych oraz już po returnie
   const [isLoading, setIsLoading] = useState(false);
   const [workers, setWorkes] = useState([]);
+  const [refresh, setRefresh] = useState(0);
 
   //pierwszy parametr useEffect to funkcja, drugi to tablica zależności - sa jej trzy opcje
   // 1. może jej nie być, to sprawi że useEffect odpali sie za kazdym razem kiedy komponent sie renderuje
@@ -36,6 +23,14 @@ function App() {
       .finally(() => {
         setIsLoading(false);
       });
+  }, [refresh]);
+
+  //możemy dodatkowo sprawić za pomocą setInterval aby odświeżało liste co jakiś konkretny czas, do tego musimy stworzyć nowego useState
+
+  useEffect(() => {
+    setInterval(() => {
+      setRefresh((old) => old + 1);
+    }, 10000);
   }, []);
 
   return (
