aa81ccb92fdbd848cb278ae23582f32b8f99b25a
Luka Ku <lukaszkuc93@gmail.com>
2022-01-25 14:35:51 +0100

    add home page and styles

--- recruting-task/src/components/App.js
+++ recruting-task/src/components/App.js
@@ -6,9 +6,20 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { PluralPhoto, PostPhoto, ToDoPhoto } from "./UserPhotos";
 
 function App() {
+  const HomePage = () => {
+    return (
+      <div className="welcomeMyApp">
+        <h1>Welcome to my APP</h1>
+      </div>
+    );
+  };
+
   return (
-    <div className="navigation">
+    <div className="first-page">
       <ul className="nav-list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
         <li>
           <Link to="/users">
             Users
@@ -28,7 +39,10 @@ function App() {
           </Link>
         </li>
       </ul>
+      <Link to="/"></Link>
+
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/users" element={<Users />} />
         <Route path="/posts" element={<Posts />} />
         <Route path="/todos" element={<ToDos />} />
