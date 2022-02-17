356f1a1edf7b306af3828c53c6f8ae647f30222f
Luka Ku <lukaszkuc93@gmail.com>
2022-02-17 16:54:03 +0100

    game memory

---
+++ React/react-memory-game-typescript/src/App.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import Card from "./components/Card/Card";
+//setup
+import { createBoard } from "./setup";
+import { shuffleArray } from "./utils";
+
+//types
+import { CardType } from "./setup";
+// Styles
+import { Grid } from "./App.styles";
+
+const App = () => {
+    // stan jest tablicą z 16 cartami które są w funkcji shuffleArray która z kolei będzie je mapowała, sortowała i jeszcze raz mapowała
+    const [cards, setCards] = useState<CardType[]>(shuffleArray(createBoard()));
+
+    //określi czy wygraliśmy grę
+    const [gameWon, setGameWon] = useState(false);
+
+    // liczba dopasowych par/card - bedzie musialo byc 8 bo mamy 16 card
+    const [countMatchedCards, setCountMatchedCards] = useState(0);
+
+    // kliknięta karta
+    const [clickedCard, setClickedCard] = useState<undefined | CardType>(
+        undefined
+    );
+
+    useEffect(() => {
+        if (countMatchedCards === cards.length / 2) {
+            console.log("wygrałeś!");
+            setGameWon(true);
+        }
+    }, [countMatchedCards]);
+    const handCardClick = (currentClickedCard: CardType) => {
+        //flipping cards
+        setCards((prev) =>
+            prev.map((card) =>
+                card.id === currentClickedCard.id
+                    ? { ...card, flipped: true, clickable: false }
+                    : card
+            )
+        );
+        // funkcja która pierwsza klikniętą kartę "currentClickedCard" wrzuci do clickedCard
+        if (!clickedCard) {
+            setClickedCard({ ...currentClickedCard });
+            console.log(clickedCard);
+            return;
+        }
+        //JEŚLI PASUJE funkcja która się uruchomi jeśli dopiero drugie lub kolejne kliknięcie będzie pasować do tego całkowicie pierwszego
+        if (clickedCard.matchingCardId === currentClickedCard.id) {
+            setCountMatchedCards((prev) => prev + 1);
+            console.log(countMatchedCards);
+            setCards((prev) =>
+                prev.map((card) =>
+                    card.id === clickedCard.id ||
+                    card.id === currentClickedCard.id
+                        ? { ...card, clickable: false }
+                        : card
+                )
+            );
+            setClickedCard(undefined);
+            return;
+        }
+        // jeśli para do siebie nie pasuje, to odwróc ja z powrotem
+        setTimeout(() => {
+            setCards((prev) =>
+                prev.map((card) =>
+                    card.id === clickedCard.id ||
+                    card.id === currentClickedCard.id
+                        ? { ...card, flipped: false, clickable: true }
+                        : card
+                )
+            );
+        }, 1000);
+        setClickedCard(undefined);
+    };
+
+    console.log(createBoard());
+    return (
+        <>
+            <Grid>
+                {cards.map((card) => {
+                    return (
+                        <Card
+                            key={card.id}
+                            card={card}
+                            callback={handCardClick}
+                        />
+                    );
+                })}
+            </Grid>
+        </>
+    );
+};
+
+export default App;
---
+++ React/react-memory-game-typescript/src/setup.ts
@@ -0,0 +1,50 @@
+//importuje wszystkie karty
+import card1 from "./img/card_1.jpg";
+import card2 from "./img/card_2.jpg";
+import card3 from "./img/card_3.jpg";
+import card4 from "./img/card_4.jpg";
+import card5 from "./img/card_5.jpg";
+import card6 from "./img/card_6.jpg";
+import card7 from "./img/card_7.jpg";
+import card8 from "./img/card_8.jpg";
+
+//import cardback
+import cardBack from "./img/card_back.jpg";
+
+// tworzenie typu dla każdej pojedyńczej karty
+export type CardType = {
+    id: string;
+    //flip czy jest odwrócona
+    flipped: boolean;
+    backImage: string;
+    frontImage: string;
+    clickable: boolean;
+    matchingCardId: string;
+};
+
+// zapis typescript
+// umieszczenie wszystkich kart w tablicy
+const cards: string[] = [
+    card1,
+    card2,
+    card3,
+    card4,
+    card5,
+    card6,
+    card7,
+    card8,
+];
+
+export const createBoard = (): CardType[] =>
+    [...cards, ...cards].map((card, i) => ({
+        id: `card${i}`,
+        flipped: false,
+        backImage: cardBack,
+        frontImage: card,
+        clickable: true,
+        // stworzylismy drugi element gdzie bedzie pokazana druga karta z innym id do dopasowania
+        matchingCardId:
+            i < cards.length
+                ? `card${i + cards.length}`
+                : `card${i - cards.length}`,
+    }));
