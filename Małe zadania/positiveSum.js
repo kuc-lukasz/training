f86e330fa7d723baa27bb820e131f82d7ee54897
Luka Ku <lukaszkuc93@gmail.com>
2022-03-08 13:47:08 +0100

    delete lines

--- Małe zadania/positiveSum.js
+++ Małe zadania/positiveSum.js
@@ -1,24 +1,26 @@
-// Stwórz funkcję która obliczy sumę liczb dodatnich 
-// Najpierw filter powoduje że wybieramy tylko liczby dodatnie, następnie reduce sumuje nam liczby 
+// Stwórz funkcję która obliczy sumę liczb dodatnich
+// Najpierw filter powoduje że wybieramy tylko liczby dodatnie, następnie reduce sumuje nam liczby
 
-const arrayNumb = [3,4,5,1,3,-2,5,-1,-2]
+const arrayNumb = [3, 4, 5, 1, 3, -2, 5, -1, -2];
 
 // Pierwsza metoda
 // function positiveNumb(array) {
 //   const newArray = array.filter((e)=> {
-//       return e > 0     
-//   })  
+//       return e > 0
+//   })
 // return newArray.reduce((acc, curr) => {
 //     return acc + curr
 // },0)
 // }
 
-//Druga metoda 
+//Druga metoda
 function positiveNumb(array) {
- const newArray = array.filter((e) => e > 0).reduce((prv, curr) => {return prv + curr})
- return newArray
+    const newArray = array
+        .filter((e) => e > 0)
+        .reduce((prv, curr) => {
+            return prv + curr;
+        });
+    return newArray;
 }
 
-console.log(positiveNumb(arrayNumb))
-
-//filter zwróci nowy array który jest literowany przez tę funkcję
+console.log(positiveNumb(arrayNumb));
